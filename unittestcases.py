4da0d73504c9ed25872bce2c6801b5fa3123ee44
Suma30-tech <mandadisumalatha30@gmail.com>
2024-09-21 11:27:49 +0530

    Update unittestcases.py

--- unittestcases.py
+++ unittestcases.py
@@ -5,5 +5,16 @@ class MultiplyTestCase(unittest.TestCase):
   def test_1(self):
     result = multiplication(3, 4)
     self.assertEqual(result, 12)
-  if __name__ == '__main__':
+   def test_2(self):
+    result = multiplication(3, -4)
+    self.assertEqual(result, -12)
+   def test_3(self):
+    result = multiplication(-3, -4)
+    self.assertEqual(result, 12)
+   def test_4(self):
+    result = multiplication(-3, 4)
+    self.assertEqual(result, -12)
+  if__name__ == '__main__':
     unittest.main()
+  
+    
